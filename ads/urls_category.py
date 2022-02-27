8e4f166aacc645dc1e8eebd3f89e46258e7d1715
marla2021 <91145453+marla2021@users.noreply.github.com>
2022-02-27 19:05:09 +0300

    add urls_category.py

---
+++ ads/urls_category.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from ads.views import category
+
+
+urlpatterns = [
+    path('', category.CategoryListView.as_view()),
+    path('create/', category.CategoryCreateView.as_view()),
+    path('<int:pk>', category.CategoryDetailView.as_view()),
+    path('<int:pk>/update/', category.CategoryUpdateView.as_view()),
+    path('<int:pk>/delete/', category.CategoryDeleteView.as_view()),
+]
