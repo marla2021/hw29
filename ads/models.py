8e973b335343681480cd63abd50cb319f4033a3e
marla2021 <91145453+marla2021@users.noreply.github.com>
2022-02-27 18:59:00 +0300

    add commit

--- ads/models.py
+++ ads/models.py
@@ -1,3 +1,67 @@
 from django.db import models
 
-# Create your models here.
+
+class Location(models.Model):
+    name = models.CharField(max_length=100)
+    lat = models.FloatField()
+    lng = models.FloatField()
+
+    class Meta:
+        verbose_name = "Локация"
+        verbose_name_plural = "Локации"
+
+    def __str__(self):
+        return self.name
+
+
+class User(models.Model):
+    ROLES = [
+        ("member", "Пользователь"),
+        ("moderator", "Модератор"),
+        ("admin", "Админ"),
+    ]
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=50)
+    username = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+    role = models.CharField(max_length=10, choices=ROLES, default="member")
+    age = models.PositiveIntegerField()
+    location = models.ManyToManyField(Location)
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+        ordering = ["username"]
+
+    def __str__(self):
+        return self.username
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+    def __str__(self):
+        return self.name
+
+
+class Ad(models.Model):
+    name = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    price = models.PositiveIntegerField()
+    description = models.TextField(null=True)
+    is_published = models.BooleanField(default=False)
+    image = models.ImageField(upload_to="upload_image/", null=True)
+    category = models.ForeignKey(Category, on_delete=models.RESTRICT, null=True)
+
+    class Meta:
+        verbose_name = "Объявление"
+        verbose_name_plural = "Объявления"
+
+    def __str__(self):
+        return self.name
+
+
