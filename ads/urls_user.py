09d9550df75e73bd0230fdee3d90ec254ed92edd
marla2021 <91145453+marla2021@users.noreply.github.com>
2022-02-27 19:06:08 +0300

    add urls_user.py

---
+++ ads/urls_user.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from ads import views
+from ads.views import user
+
+urlpatterns = [
+    path('', user.UserView.as_view()),
+    path('create/', user.UserCreateView.as_view()),
+    path('<int:pk>', user.UserDetailView.as_view()),
+    path('<int:pk>/update/', user.UserUpdateView.as_view()),
+    path('<int:pk>/delete/', user.UserDeleteView.as_view()),
+]
