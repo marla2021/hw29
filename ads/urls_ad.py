4f56bb48036a8f07f70b4b0d96cb8c97e55926d9
marla2021 <91145453+marla2021@users.noreply.github.com>
2022-02-27 19:04:03 +0300

    add urls_ad.py

---
+++ ads/urls_ad.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+
+from ads.views import ad
+
+urlpatterns = [
+    path('', ad.AdListView.as_view()),
+    path('create/', ad.AdCreateView.as_view()),
+    path('<int:pk>/', ad.AdDetailView.as_view()),
+    path('<int:pk>/update/', ad.AdUpdateView.as_view()),
+    path('<int:pk>/delete/', ad.AdDeleteView.as_view()),
+    path('<int:pk>/upload_image/', ad.AdImageView.as_view()),
+]
