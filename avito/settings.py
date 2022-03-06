c151c0ba3edb28b77a9adb53edf19b745632dfd3
marla2021 <91145453+marla2021@users.noreply.github.com>
2022-03-06 19:10:24 +0300

    add settings

--- avito/settings.py
+++ avito/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'rest_framework_simplejwt',
     'ads',
     'authentication'
 ]
@@ -138,7 +139,11 @@ TOTAL_ON_PAGE = 10
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ]
 }
 
 # LOGGING = {
