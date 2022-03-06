08acc161e0712c616374a20b62cd458ee9e00e0c
marla2021 <91145453+marla2021@users.noreply.github.com>
2022-03-06 19:11:55 +0300

    add urls token

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from authentication.views import UserAuthCreateView
 
 urlpatterns = [
     path('create/', UserAuthCreateView.as_view()),
+    path('token/', TokenObtainPairView.as_view()),
+    path('token/refresh/', TokenRefreshView.as_view()),
 ]
