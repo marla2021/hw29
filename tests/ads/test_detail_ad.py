39f37fe44dfae16865267f59d498d3e18e3a8589
marla2021 <91145453+marla2021@users.noreply.github.com>
2022-03-21 21:12:08 +0300

    add test

--- tests/ads/test_detail_ad.py
+++ tests/ads/test_detail_ad.py
@@ -7,9 +7,9 @@ def test_detail_ad(client, ad, user_token):
         f"/ad/{ad.id}/",
         content_type="application/json",
         HTTP_AUTHORIZATION=f"Bearer {user_token}")
-
-    assert response.status_code == 200
-    assert response.json() == {
+    if ad.id:
+        assert response.status_code == 200
+        assert response.json() == {
         "id": ad.id,
         "is_published": ad.is_published,
         "name": ad.name,
@@ -18,4 +18,6 @@ def test_detail_ad(client, ad, user_token):
         "image": ad.image.url if ad.image else None,
         "author": ad.author_id,
         "category": ad.category_id
-    }
+        }
+    else:
+        assert response.status_code == 404
